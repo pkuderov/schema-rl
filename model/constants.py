4b035a4974cadcc2daa47a6c2949355e821990c9
aleksandrashlychkova <aleksandrashlychkova@gmail.com>
2019-11-01 16:57:06 +0300

    state where schemas are learned normally

--- model/constants.py
+++ model/constants.py
@@ -10,17 +10,15 @@ class Constants:
 
     VISUALIZE_SCHEMAS = True
     VISUALIZE_INNER_STATE = True
-    VISUALIZE_BACKTRACKING = True
-    VISUALIZE_STATE = True
 
     if not DEBUG:
+        SCREEN_WIDTH = 94
         SCREEN_HEIGHT = 117
-        SCREEN_WIDTH = 94  # using 5 brick columns
 
         N = SCREEN_WIDTH * SCREEN_HEIGHT
         M = 5
-        T = 112  # was 50  # min 112; better 130
-        ACTION_SPACE_DIM = 3
+        T = 10
+        ACTION_SPACE_DIM = 2
         REWARD_SPACE_DIM = 2
 
         NEIGHBORHOOD_RADIUS = 2
@@ -29,39 +27,30 @@ class Constants:
         SCREEN_HEIGHT = 3
 
         N = 9  # SCREEN_WIDTH * SCREEN_HEIGHT
-        M = 2
-        T = 16
+        M = 1
+        T = 50
         ACTION_SPACE_DIM = 2
         REWARD_SPACE_DIM = 2
 
         NEIGHBORHOOD_RADIUS = 1
 
-    EP_NUM = 100
+    EP_NUM = 10
     L = 1000
     FILTER_SIZE = 2 * NEIGHBORHOOD_RADIUS + 1
     NEIGHBORS_NUM = FILTER_SIZE ** 2 - 1
 
     FAKE_ENTITY_IDX = N
     EPSILON = 0
-
     FRAME_STACK_SIZE = 2
+
     SCHEMA_VEC_SIZE = FRAME_STACK_SIZE * (M * (NEIGHBORS_NUM + 1)) + ACTION_SPACE_DIM
-    TIME_SIZE = FRAME_STACK_SIZE + T
 
     # indices of corresponding attributes in entities' vectors
     BALL_IDX = 0
     PADDLE_IDX = 1
     WALL_IDX = 2
     BRICK_IDX = 3
-    if not DEBUG:
-        VOID_IDX = 4
-    else:
-        VOID_IDX = 1
-
-    # action indices
-    ACTION_NOP = 0
-    ACTION_MOVE_LEFT = 1
-    ACTION_MOVE_RIGHT = 2
+    VOID_IDX = 4
 
     ENTITY_NAMES = {
         BALL_IDX: 'BALL',
@@ -74,11 +63,3 @@ class Constants:
         0: 'POSITIVE',
         1: 'NEGATIVE',
     }
-
-"""
-env changed constants:
-DEFAULT_NUM_BRICKS_COLS = 11
-BOUNCE_STOCHASTICITY = 0.25
-PADDLE_SPEED_DISTRIBUTION[-1] = 0.90
-PADDLE_SPEED_DISTRIBUTION[-2] = 0.10
-"""
